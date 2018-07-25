b6f75312c905b378e081cf90514d708e028d57ac
Bhavin Patel <mailmebhav@gmail.com>
2018-07-25 10:27:15 +0530

    Moved GDPAggregateTest class to AggregateGDPPopulationTest.cs

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,9 +1,42 @@
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
 namespace AggregateGDPPopulation.Tests
 {
+    public class GDPAggregateTest
+    {
+        //This function is used for testing to check the values of gdp and population by continent.
+        public static bool AggregatedGDP(string expectedOutput, string originalOutput)
+        {
+            bool errorMatching = false;
+            Dictionary<string, Dictionary<string, float>> expectedOutputDictionary = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, float>>>(expectedOutput);
+            Dictionary<string, Dictionary<string, float>> OriginalOutputDictionary = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, float>>>(originalOutput);
+            foreach (string continent in expectedOutputDictionary.Keys)
+            {
+                if (expectedOutputDictionary[continent]["GDP_2012"] == OriginalOutputDictionary[continent]["GDP_2012"])
+                {
+                    if (expectedOutputDictionary[continent]["POPULATION_2012"] != expectedOutputDictionary[continent]["POPULATION_2012"])
+                    {
+                        errorMatching = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    errorMatching = true;
+                    break;
+                }
+            }
+            if (errorMatching)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
     public class AggregateGDPPopulationTest
     {
         [Fact]
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -27,38 +27,6 @@ namespace AggregateGDPPopulation
             }
         }
     }
-    public class GDPAggregateTest
-    {
-        //This function is used for testing to check the values of gdp and population by continent.
-        public static bool AggregatedGDP(string expectedOutput, string originalOutput)
-        {
-            bool errorMatching = false;
-            Dictionary<string, Dictionary<string, float>> expectedOutputDictionary = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, float>>>(expectedOutput);
-            Dictionary<string, Dictionary<string, float>> OriginalOutputDictionary = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, float>>>(originalOutput);
-            foreach (string continent in expectedOutputDictionary.Keys)
-            {
-                if (expectedOutputDictionary[continent]["GDP_2012"] == OriginalOutputDictionary[continent]["GDP_2012"])
-                {
-                    if (expectedOutputDictionary[continent]["POPULATION_2012"] != expectedOutputDictionary[continent]["POPULATION_2012"])
-                    {
-                        errorMatching = true;
-                        break;
-                    }
-                }
-                else
-                {
-                    errorMatching = true;
-                    break;
-                }
-            }
-            if (errorMatching)
-            {
-                return false;
-            }
-            return true;
-        }
-    }
-
     public class GDPAggregate
     {
         //Adding values of GDP and Population of a Country to the existing continent data in finaljson.
